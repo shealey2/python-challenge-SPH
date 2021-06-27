f7a30b61762120f2962f0c3335230b6bdb98bf48
shealey2 <shealey2@alumni.nd.edu>
2021-06-26 17:02:54 -0700

    Added in total number of months

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,21 @@
 import os
 import csv
 
-cwd = os.getcwd()
 csvpath = os.path.join('c:/Users/Sean & Lauren/DataClass/python-challenge-SPH/PyBank/','Resources','budget_data.csv')
 
 
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    num_months = 0
 
+    next(csvreader, None)
     for row in csvreader:
-        print(row[1])
+       
+        num_months += 1
+    
+        
+    print(f"The total number of months is {num_months}.")
+    print(f"")
+
+        
