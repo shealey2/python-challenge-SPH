c1de0f1cf8c7aae17169b45ddf570ae4f49122a3
shealey2 <shealey2@alumni.nd.edu>
2021-06-26 19:33:50 -0700

    prelim completion of change in profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,16 +3,22 @@ import csv
 
 csvpath = os.path.join('c:/Users/Sean & Lauren/DataClass/python-challenge-SPH/PyBank/','Resources','budget_data.csv')
 
-
+#variable for total month
+num_months = 0
+#variable for total profit
+sum_profit = 0   
+#variables for average change
+first_month = 0
+last_month = 0
+#variables for greatest increase
+great_inc = 0
+great_month = ""
+#variables for greatest decrease
+great_dec = 0
+worst_month = ""
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-    num_months = 0
-    sum_profit = 0   
-    month_change = 0
-    first_month = 0
-    last_month = 0
     
     #Removed Header
     next(csvreader, None)
@@ -31,15 +37,26 @@ with open(csvpath) as csvfile:
             first_month = float(row[1])
         
         last_month = float(row[1])
+
+        #determines if this month profit was greater than the previous greatest and stores month and profit
+        if(float(row[1])>great_inc):
+            great_inc = float(row[1])
+            great_month = str(row[0])
+        
+        #determines if this month profit was less than the previous greatest and stores month and profit
+        if(float(row[1])<great_dec):
+            great_dec = float(row[1])
+            worst_month = str(row[0])
+
    
-    #calculates the average change across months
-    avg_change = (last_month - first_month) / (num_months - 1)    
+#calculates the average change across months
+avg_change = (last_month - first_month) / (num_months - 1)    
  
     
-    print(f"Financial Analysis")
-    print(f"......................................")
-    print(f"Total Months: {num_months}")
-    print(f"Total: {'${:,.2f}'.format(sum_profit)}")
-    print(f"Average Change: {'${:,.2f}'.format(avg_change)}")
-
-        
+print(f"Financial Analysis")
+print(f"......................................")
+print(f"Total Months: {num_months}")
+print(f"Total: {'${:,.2f}'.format(sum_profit)}")
+print(f"Average Change: {'${:,.2f}'.format(avg_change)}")
+print(f"Greatest Increase in Profits: {great_month} ({'${:,.2f}'.format(great_inc)})")
+print(f"Greatest Increase in Profits: {worst_month} ({'${:,.2f}'.format(great_dec)})")
