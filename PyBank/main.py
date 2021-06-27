740075e5c0f7def6714901f0cb2586e622cf0545
shealey2 <shealey2@alumni.nd.edu>
2021-06-26 17:14:13 -0700

    added in total profit calc

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,14 +8,17 @@ csvpath = os.path.join('c:/Users/Sean & Lauren/DataClass/python-challenge-SPH/Py
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     num_months = 0
+    sum_profit = 0
 
     next(csvreader, None)
     for row in csvreader:
        
         num_months += 1
+        sum_profit += float(row[1])
     
-        
-    print(f"The total number of months is {num_months}.")
-    print(f"")
+    print(f"Financial Analysis")
+    print(f"......................................")
+    print(f"Total Months: {num_months}")
+    print(f"Total: {'${:,.2f}'.format(sum_profit)}")
 
         
