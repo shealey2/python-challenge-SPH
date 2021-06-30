3b908ab0159c02327bf21b90a49df6efc3bcb600
shealey2 <shealey2@alumni.nd.edu>
2021-06-29 22:55:14 -0700

    completed total votes, unique candidates and votes

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 
 
+#change file path if necessary 
 csvpath = os.path.join('c:/Users/Sean & Lauren/DataClass/python-challenge-SPH/PyBank/','Resources','budget_data.csv')
 output_path = os.path.join('c:/Users/Sean & Lauren/DataClass/python-challenge-SPH/PyBank/','analysis','analysis.txt')
 
